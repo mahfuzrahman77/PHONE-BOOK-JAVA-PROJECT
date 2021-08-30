85f6080299aa1dfb7aa43023fed25b0cf8e9c065
mahfuzrahman77 <mahfuzrahman67@yahoo.com>
2021-08-30 22:57:27 +0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Phone-book-java-project
+ Phone Book is a very useful application . The feature of this simple application includes adding, updating, deleting, viewing and searching a list of telephone subscribers/members.
---
+++ backup.sql
@@ -0,0 +1,5 @@
+insert into notebook.table2 values ('ibu' , 'Narsingdi' , '015555555');
+insert into notebook.table2 values ('Mahfuz Rahman' , 'Dhaka' , '01585555555');
+insert into notebook.table2 values ('Sydul Islam' , 'Dhaka' , '01880808019');
+insert into notebook.table2 values ('abc' , 'Dhaka' , '123456');
+insert into notebook.table2 values ('korim' , 'Dhaka' , '56565656');
